1905c6697bf668a1f61ad5412ae91bab1df8394f
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-09-21 18:43:18 +0200

    update category

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,11 +1,10 @@
 const Category = require('../models/categories')
 
-exports.createCategories = (req, res) => {
+exports.createCategory = (req, res) => {
     verifyIsAdmin(req)
     const categories = new Category({
         name: req.body.name,
     });
-    console.log(categories)
     categories.save()
     .then(categories => { res.status(200).json({ categories})})
     .catch(error => { res.status(400).json( { error })})
@@ -20,12 +19,8 @@ exports.getAllCategories = (req, res) => {
 
 exports.deleteCategory = async (req, res) => {
     try {
-        const { id } = req.params;
-        const category = await Category.findById(id);
         verifyIsAdmin(req)
-        if (!category) {
-            return res.status(404).json({ message: 'Catégorie non trouvée' });
-        }
+        const category = await verifyExistingCategory(req, res)
         await category.deleteOne();
         return res.status(200).json({ message: 'Catégorie supprimée avec succès' });
     } catch (error) {
@@ -33,8 +28,33 @@ exports.deleteCategory = async (req, res) => {
     }
 };
 
+exports.updateCategory = async (req, res) => {
+  try {
+    const { name } = req.body; 
+    verifyIsAdmin(req)
+    const category = await verifyExistingCategory(req, res)
+    category.name = name;
+    const updatedCategory = await category.save();
+    return res.status(200).json({ message: 'Catégorie mise à jour avec succès', category: updatedCategory });
+  } catch (error) {
+    return res.status(500).json({ message: 'Erreur lors de la mise à jour de la catégorie', error: error.message });
+  }
+};
+
 function verifyIsAdmin(req) {
     if(!req.auth || req.auth.role !== 'admin') {
-        return res.status(403).json({ message: 'Requête non autorisée' });
+        const error = new Error('Requête non autorisée');
+        error.status = 403;
+        throw error; 
+    }
+}
+
+async function verifyExistingCategory(req, res) {
+    const { id } = req.params; 
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: 'Catégorie non trouvée' });
+    } else {
+        return category;
     }
 }
--- routes/categories.js
+++ routes/categories.js
@@ -4,8 +4,9 @@ const categoriesController = require('../controllers/categories');
 const authenticated = require('../middlewares/authenticated');
 const auth = require('../middlewares/auth');
 
-router.post('/', auth, authenticated, categoriesController.createCategories);
+router.post('/', auth, authenticated, categoriesController.createCategory);
 router.get('/', auth, authenticated, categoriesController.getAllCategories);
 router.delete('/:id', auth, authenticated, categoriesController.deleteCategory);
+router.put('/:id', auth, authenticated, categoriesController.updateCategory);
 
 module.exports = router;
