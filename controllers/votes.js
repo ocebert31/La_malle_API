b03111f5f5ed597e94686fdc2b1c2b8c2f7f6f60
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-10-06 19:35:32 +0200

    refacto vote

--- controllers/votes.js
+++ controllers/votes.js
@@ -7,25 +7,32 @@ exports.createVote = async (req, res) => {
     const commentId = req.body.commentId || null;
     const articleId = req.body.articleId || null;
     const voteParams = _.pickBy({ userId, commentId, articleId }, value => value !== null);
-
+    const paramsForVote = {voteType, userId, commentId, articleId}
     try {
         const existingVote = await Vote.findOne(voteParams);
-        if (existingVote) {
-            await Vote.deleteOne({ _id: existingVote._id });
-            if (existingVote.voteType === voteType) {
-                return res.status(200).json({ message: 'Vote supprimé avec succès' });
-            } else {
-                const vote = new Vote({ userId, commentId, articleId, voteType });
-                await vote.save();
-                return res.status(200).json({ message: 'Vote changé avec succès', vote });
-            }
-        } else {
-            const vote = new Vote({ userId, commentId, articleId, voteType });
-            await vote.save();
-            return res.status(201).json({ message: 'Vote enregistré avec succès', vote });
-        }
+        await checkUserAlreadyVoted(existingVote, paramsForVote, res)
     } catch (error) {
-        console.error(error);
         res.status(500).json({ error: error.message });
     }
 };
+
+async function checkUserAlreadyVoted(existingVote, paramsForVote, res) {
+    if (existingVote) {
+        await handleExistingVote(existingVote, paramsForVote, res)
+    } else {
+        const vote = new Vote(paramsForVote);
+        await vote.save();
+        return res.status(201).json({ message: 'Vote enregistré avec succès', vote });
+    }
+}
+
+async function handleExistingVote(existingVote, paramsForVote, res) {
+    await Vote.deleteOne({ _id: existingVote._id });
+    if (existingVote.voteType === paramsForVote.voteType) {
+        return res.status(200).json({ message: 'Vote supprimé avec succès' });
+    } else {
+        const vote = new Vote(paramsForVote);
+        await vote.save();
+        return res.status(200).json({ message: 'Vote changé avec succès', vote });
+    }
+}
