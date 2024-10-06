a88052c57c421f02285a00d24ab858953dd4ece3
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-10-06 19:07:51 +0200

    refacto middleware auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
-module.exports = async (req, res, next) => {
+async function auth (req, res, next) {
     try {
         if (req.headers.authorization) {
             const token = req.headers.authorization.split(' ')[1];
             const decodedToken = jwt.verify(token, process.env.AUTH_TOKEN);
             const user = await User.findById(decodedToken.userId);
-            if (user) {
-                req.auth = {
-                    userId: user._id,
-                    role: user.role,
-                    pseudo: user.pseudo
-                };
-            } 
+            setAuthInRequest(user, req)
         }
         next();
     } catch (error) {
@@ -21,3 +15,15 @@ module.exports = async (req, res, next) => {
         next();
     }
 };
+
+function setAuthInRequest(user, req) {
+    if (user) {
+        req.auth = {
+            userId: user._id,
+            role: user.role,
+            pseudo: user.pseudo
+        };
+    } 
+}
+
+module.exports = auth;
