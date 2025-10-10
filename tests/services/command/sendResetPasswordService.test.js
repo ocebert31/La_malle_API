9f2275ce226437aeb6d93e4a7fb5884a3cb33b9e
Océane Bertrand <oceanebertrand339@gmail.com>
2025-10-10 21:05:54 +0200

    test: simulate failing test to check CI

--- tests/services/command/sendResetPasswordService.test.js
+++ tests/services/command/sendResetPasswordService.test.js
@@ -54,7 +54,7 @@ describe('sendResetPasswordService', () => {
         expect(html).toContain('&copy; 2025 La malle. Tous droits réservés.');
     });
 
-    it('should fail on purpose to test CI', () => {
+    it('should fail on purpose to test CI...', () => {
         expect(true).toBe(false);
     });
 });
